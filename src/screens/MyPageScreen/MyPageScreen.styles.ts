0fee719d54ba51371f80921ca078c3ec3a96a49e
김미르 <samkmr0220@gmail.com>
2026-02-04 17:24:11 +0900

    리팩터링: 미사용 코드 제거 (MyPage profile 스타일, filterTodayActiveTodoLists)

--- src/screens/MyPageScreen/MyPageScreen.styles.ts
+++ src/screens/MyPageScreen/MyPageScreen.styles.ts
@@ -5,7 +5,7 @@
 
 import { StyleSheet } from 'react-native';
 import { colors, spacing, typography, borderRadius, shadows, layout } from '../../utils/designTokens';
-import { createTextStyle, createTitleStyle, createSecondaryTextStyle } from '../../utils/styles/textStyles';
+import { createTextStyle, createTitleStyle } from '../../utils/styles/textStyles';
 
 export const styles = StyleSheet.create({
   container: {
